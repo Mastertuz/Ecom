80c6bee9d43f77276229b502966e54f937fd0c43
Mastertuz <futureprogrammer2301@gmail.com>
2025-06-06 12:25:54 +0300

    edited sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -9,51 +9,42 @@ const Page = async () => {
   if (session) redirect("/");
 
   return (
-    <div className="w-full max-w-sm mx-auto space-y-6">
-      <h1 className="text-2xl font-bold text-center mb-6">Sign In</h1>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-sm space-y-6">
+        <h1 className="text-2xl font-bold text-center mb-6">Авторизация</h1>
 
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
+        {/* Email/Password Sign In */}
+        <form
+          className="space-y-4"
+          action={async (formData) => {
+            "use server";
+            await signIn("credentials", formData);
+          }}
+        >
+          <Input
+            name="email"
+            placeholder="Электронная почта"
+            type="email"
+            required
+            autoComplete="email"
+          />
+          <Input
+            name="password"
+            placeholder="Пароль"
+            type="password"
+            required
+            autoComplete="current-password"
+          />
+          <Button className="w-full cursor-pointer" type="submit">
+            Войти
+          </Button>
+        </form>
+
+        <div className="text-center">
+          <Button asChild variant="link" >
+            <Link href="/sign-up" className="cursor-pointer">Нет аккаунта? Зарегистрироваться</Link>
+          </Button>
         </div>
-        <div className="relative flex justify-center text-sm">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with email
-          </span>
-        </div>
-      </div>
-
-      {/* Email/Password Sign In */}
-      <form
-        className="space-y-4"
-        action={async (formData) => {
-          "use server";
-              await signIn("credentials", formData);
-        }}
-      >
-        <Input
-          name="email"
-          placeholder="Email"
-          type="email"
-          required
-          autoComplete="email"
-        />
-        <Input
-          name="password"
-          placeholder="Password"
-          type="password"
-          required
-          autoComplete="current-password"
-        />
-        <Button className="w-full" type="submit">
-          Sign In
-        </Button>
-      </form>
-
-      <div className="text-center">
-        <Button asChild variant="link">
-          <Link href="/sign-up">Don&apos;t have an account? Sign up</Link>
-        </Button>
       </div>
     </div>
   );
