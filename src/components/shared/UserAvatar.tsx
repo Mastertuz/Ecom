769e1b26428de2fa3599c8b66eb119501928b178
Mastertuz <futureprogrammer2301@gmail.com>
2025-06-18 19:37:36 +0300

    fixed user avatar

--- src/components/shared/UserAvatar.tsx
+++ src/components/shared/UserAvatar.tsx
@@ -1,21 +1,17 @@
-import Image from "next/image"
-import { auth } from "../../../auth"
- 
-export default async function UserAvatar() {
-  const session = await auth()
- 
-  if (!session?.user) return null
-  const isAdmin = session.user.role === 'admin'
+import Image from "next/image";
+import { auth } from "../../../auth";
+import { AvatarImage,Avatar } from "../ui/avatar";
 
+export default async function UserAvatar() {
+  const session = await auth();
+
+  if (!session?.user) return null;
   return (
     <div className="cursor-pointer hover:opacity-80 transition-opacity">
-      <Image 
-        src={`${isAdmin?'/user-gear.png':'/user.png'}`} 
-        width={24} 
-        height={24}
-        className="size-10 rounded-full object-cover" 
-        alt="User Avatar" 
-      />
+      <Avatar className="size-10">
+        <AvatarImage  src={session.user.image || 'user.png'}/>
+      </Avatar>
+      
     </div>
-  )
+  );
 }
