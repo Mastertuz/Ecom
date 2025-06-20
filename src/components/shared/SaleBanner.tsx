25469bd6a8c59bff43307cfe4439799e1103c2f0
Mastertuz <futureprogrammer2301@gmail.com>
2025-06-20 17:56:09 +0300

    still fixing

--- src/components/shared/SaleBanner.tsx
+++ src/components/shared/SaleBanner.tsx
@@ -1,4 +1,3 @@
-
 import { PromoCode } from "../../../typings"
 import CopyButton from "./CopyButton"
 
@@ -10,17 +9,21 @@ interface SaleBannerData {
 
 async function getPromoData(): Promise<SaleBannerData | null> {
   try {
-     const baseUrl =
-      process.env.NEXT_PUBLIC_BASE_URL ??
-      (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
+    const baseUrl = process.env.NODE_ENV=== 'production'?
+    process.env.VERCEL_URL 
+    :
+    process.env.NEXT_PUBLIC_BASE_URL
 
-    const res = await fetch(`${baseUrl}/api/promo/active`);
+    const res = await fetch(`${baseUrl}/api/promo/active`)
 
-    if (!res.ok) return null
+    if (!res.ok) {
+      console.error('Failed to fetch promo data:', res.status)
+      return null
+    }
 
-    return res.json()
+    return await res.json()
   } catch (error) {
-    console.error("Ошибка получения промо-данных:", error)
+    console.error("Error fetching promo data:", error)
     return null
   }
 }
@@ -41,7 +44,9 @@ export default async function SaleBanner() {
     <div className="bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mt-2 max-sm:px-4 max-sm:py-6 rounded-lg shadow-lg">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex-1">
-          <h2 className="text-5xl font-extrabold text-left mb-4 max-lg:text-3xl max-sm:text-2xl">{bannerData.title}</h2>
+          <h2 className="text-5xl font-extrabold text-left mb-4 max-lg:text-3xl max-sm:text-2xl">
+            {bannerData.title}
+          </h2>
           <p className="text-left text-3xl font-semibold mb-6 max-lg:text-xl max-sm:text-lg">
             {bannerData.description}
           </p>
@@ -49,8 +54,7 @@ export default async function SaleBanner() {
           <div className="flex">
             <div className="bg-white text-black py-4 px-6 max-sm:py-2 max-sm:px-4 rounded-full shadow-md transform hover:scale-105 transition duration-300">
               <span className="font-bold text-xl max-lg:text-lg max-sm:text-base">
-                Промокод:{" "}
-                <CopyButton code={promoCode.code} />
+                Промокод: <CopyButton code={promoCode.code} />
               </span>
               <span className="ml-2 font-bold text-xl max-lg:text-lg max-sm:text-base">
                 на скидку {promoCode.discount}%
